64d45e8f57110d76b93ba5d96d330c60da86c5c3
standera <47768943+tableaublueprint@users.noreply.github.com>
2020-07-23 13:28:16 +0800

    original

---
+++ herokuwdc.js
@@ -0,0 +1,70 @@
+(function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
+
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [{
+            id: "firstname",
+            alias: "First Name",
+            dataType: tableau.dataTypeEnum.string
+        }
+        ];
+
+        var tableSchema = {
+            id: "HerokuMule",
+            alias: "HerokuMule MFG Dataset",
+            columns: cols
+        };
+
+        schemaCallback([tableSchema]);
+    };
+
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+
+     /*   $.ajaxSetup({
+         headers : {
+            'x-rapidapi-host' : 'api-nba-v1.p.rapidapi.com',
+            'x-rapidapi-key' : 'd18cdeb3a2mshab6824adf713c9ep130fb2jsncca2d444588d'
+          } 
+        }); */
+
+        $.getJSON("http://localhost:8081/post", function(resp) {
+            var feat = resp.raw_data,
+                tableData = [];
+            
+            // Iterate over the JSON object
+             for (var j = 0, len = feat.length; j < len; j++) { 
+                
+
+               
+                    tableData.push({
+                    
+                        "firstname" : feat[j].firstname,                       
+                        
+                    });        
+
+
+                       
+
+                
+            } 
+            
+                    
+            
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
+
+    tableau.registerConnector(myConnector);
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "HerokuMule MFG Dataset"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
+})();
